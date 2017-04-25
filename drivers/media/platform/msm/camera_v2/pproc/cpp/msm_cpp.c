836ec6d43627ddf559eafc3616b11c6fcd6096f8
Slim80 <bestboss1980@gmail.com>
2017-04-25 21:24:41 +0200

    Patches ported from the last H811 v20u sources

--- drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
+++ drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
@@ -1924,16 +1924,16 @@ static int msm_cpp_cfg_frame(struct cpp_device *cpp_dev,
 		return -EINVAL;
 	}
 
+    if (stripe_base == UINT_MAX || new_frame->num_strips >
+        (UINT_MAX - 1 - stripe_base) / stripe_size) {
+        pr_err("Invalid frame message,num_strips %d is large\n",
+            new_frame->num_strips);
+        return -EINVAL;
+    }
+
 	if (cpp_frame_msg[new_frame->msg_len - 1] !=
 		MSM_CPP_MSG_ID_TRAILER) {
 		pr_err("Invalid frame message\n");
-		return -EINVAL;
-	}
-
-	if (stripe_base == UINT_MAX || new_frame->num_strips >
-		(UINT_MAX - 1 - stripe_base) / stripe_size) {
-		pr_err("Invalid frame message,num_strips %d is large\n",
-			new_frame->num_strips);
 		return -EINVAL;
 	}
 
